eef7f207cbdd52659ec382e98d9077c3c818ae03
Hieu Nguyen <hnnguyen45@gmail.com>
2016-09-10 00:04:15 -0700

    starting and out and first commit.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
-
+//Starting out with the document ready function to create the application javascript.
 $(document).ready(function(){
-	
+	//Given already now to create the game.
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
 
   	});
-
+  	//Given already now to create the game.
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+  	//Starting to input and create javascript coding and also jQuery below...
+
 
 });
 

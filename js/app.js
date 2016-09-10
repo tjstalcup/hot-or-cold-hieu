13dd92c07c0be8fb538746afba7d8cb55edac6bd
Hieu Nguyen <hnnguyen45@gmail.com>
2016-09-10 03:27:51 -0700

    Almost Complete with hot or cold app. Need to fix a few things.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,74 @@
-//Starting out with the document ready function to create the application javascript.
+
 $(document).ready(function(){
-	//Given already now to create the game.
+	
+	var chosenNumber = Math.floor((Math.random() * 100) + 1);
+	//console.log(chosenNumber);
+	var count = 0;
+
+	var guess = 0;
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
 
   	});
-  	//Given already now to create the game.
+
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	//Starting to input and create javascript coding and also jQuery below...
+  	$("input.button").click(function(event){
+  		event.preventDefault();
+  		guess= $("#userGuess").val();
+  			if (guess>0 && guess%1===0 && guess<100){
+  				$("ul.guessBox").append("<li>"+ guess +"</li>");
+  				$("#userGuess").val("");
+  				$("#count").text(count+1);
+  				count =+ 1;
+  				testNumber();
+  			}
+  			else {
+  				window.alert("Please enter a valid integer between 1 and 100.");
+  				$("#userGuess").val("");
+  			}
+  	});
 
+  	$(".new").click(function(){
+  		newGame();
+  	});
+
+  	function testNumber(){
+  		//console.log(guess);
+  		if (Math.abs(chosenNumber-guess)>=50){
+  			$("#feedback").text("Ice Cold");
+  		}
+  		else if (Math.abs(chosenNumber-guess)<50 && Math.abs(chosenNumber-guess)>=30){
+  			$("#feedback").text("Cold");
+  		}
+  		else if (Math.abs(chosenNumber-guess)>=20 && Math.abs(chosenNumber-guess)<30){
+  			$("#feedback").text("warm");
+  		}
+  		else if (Math.abs(chosenNumber-guess)<20 && Math.abs(chosenNumber-guess)>=10){
+  			$("#feedback").text("hot");
+  		}
+  		else if (Math.abs(chosenNumber-guess)<10 && Math.abs(chosenNumber-guess)!==0){
+  			$("#feedback").text("very hot");
+  		}
+  		else{
+  			$("#feedback").text("YOU GOT IT! YAY!");
+  		}
+  	}
+
+  	function newGame(){
+  		chosenNumber = Math.floor((Math.random() * 100) + 1);
+		//console.log(chosenNumber);
+		count = 0, guess = 0;
+		$("#feedback").text("Make your Guess!");
+		$("#count").text(count);
+		$("#userGuess").val("");
+		$("ul.guessBox").empty();
+  	}
 
 });
 
